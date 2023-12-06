a998422c13a2cbf41ce7b500e575ca077d8050dd
Lucas S <Lucasluke307@gmail.com>
2023-12-06 16:00:16 -0300

    Adicionando slider

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
+import Slider from "@react-native-community/slider";
 import { StyleSheet, Text, View } from "react-native";
-
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Teste</Text>
+      {/* <Image source={require("./src/assets/logo.png")} style={styles.logo} /> */}
+      <Text style={styles.logo}>Password Generator</Text>
+
+      <Text style={styles.title}>20 Caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider style={{ height: 50 }} minimumValue={6} maximumValue={20} />
+      </View>
     </View>
   )
 }
@@ -15,5 +22,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#f3f3ff",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 30,
+    marginBottom: 60,
+  },
+  area:{
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#fff"
   }
 });
